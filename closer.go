36855b3b4ee03c7099a0e3bdc1b39fd547299113
akm <akm2000@gmail.com>
2017-01-17 20:53:37 +0900

    Dummy implementation of Closer

--- closer.go
+++ closer.go
@@ -2,13 +2,22 @@ package pipeline
 
 import (
 	"golang.org/x/net/context"
-	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/datastore"
+	// "google.golang.org/appengine/log"
 )
 
 type Closer struct {
 }
 
 func (b *Closer) Process(ctx context.Context, pl *Pipeline) error {
-	log.Debugf(ctx, "Closing pipeline %v\n", pl)
+	pl.Props.Status = closed
+	key, err := datastore.DecodeKey(pl.ID)
+	if err != nil {
+		return err
+	}
+	_, err = datastore.Put(ctx, key, &pl.Props)
+	if err != nil {
+		return err
+	}
 	return nil
 }
