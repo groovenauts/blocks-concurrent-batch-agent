4027533c1b1adb49464e8f9374b2ca96308e11cb
akm <akm2000@gmail.com>
2017-01-27 19:40:21 +0900

    Implement Refresher
    Implement Refresher

--- pipeline.go
+++ pipeline.go
@@ -30,6 +30,19 @@ var processorFactory ProcessorFactory = &DefaultProcessorFactory{}
 var ErrNoSuchPipeline = errors.New("No such data in Pipelines")
 
 type (
+	// See https://godoc.org/google.golang.org/api/deploymentmanager/v2#OperationErrorErrors
+	DeploymentError struct {
+    // Code: [Output Only] The error type identifier for this error.
+    Code string `json:"code,omitempty"`
+
+    // Location: [Output Only] Indicates the field in the request that
+    // caused the error. This property is optional.
+    Location string `json:"location,omitempty"`
+
+    // Message: [Output Only] An optional, human-readable error message.
+    Message string `json:"message,omitempty"`
+	}
+
 	PipelineProps struct {
 		Name          string `json:"name"           validate:"required"`
 		ProjectID     string `json:"project_id"     validate:"required"`
@@ -43,6 +56,7 @@ type (
 		Status        Status `json:"status"`
 		Dryrun        bool   `json:"dryrun"`
 		DeploymentName string `json:"deployment_name"`
+		Errors        []DeploymentError `json:"errors"`
 	}
 
 	Pipeline struct {
--- refresher.go
+++ refresher.go
@@ -2,6 +2,8 @@ package pipeline
 
 import (
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/deploymentmanager/v2"
 	"google.golang.org/appengine/log"
 )
 
@@ -10,5 +12,53 @@ type Refresher struct {
 
 func (b *Refresher) Process(ctx context.Context, pl *Pipeline) error {
 	log.Debugf(ctx, "Refreshing pipeline %v\n", pl)
+	if pl.Props.Status == deploying {
+		b.UpdateStatusByDeployment(ctx, pl)
+	}
+	return nil
+}
+
+func (b *Refresher) UpdateStatusByDeployment(ctx context.Context, pl *Pipeline) error {
+	// See the "Examples" below "Response"
+	//   https://cloud.google.com/deployment-manager/docs/reference/latest/deployments/insert#response
+	hc, err := google.DefaultClient(ctx, deploymentmanager.CloudPlatformScope)
+	if err != nil {
+		log.Errorf(ctx, "Failed to get google.DefaultClient: %v\n", err)
+		return err
+	}
+	c, err := deploymentmanager.New(hc)
+	if err != nil {
+		log.Errorf(ctx, "Failed to get deploymentmanager.New(hc): %v\nhc: %v\n", err, hc)
+		return err
+	}
+	proj := pl.Props.ProjectID
+	dep_name := pl.Props.DeploymentName
+	deployment, err := c.Deployments.Get(proj, dep_name).Context(ctx).Do()
+	if err != nil {
+		log.Errorf(ctx, "Failed to get deployment %v\nproject: %v deployment: %v\nhc: %v\n", err, proj, dep_name)
+		return err
+	}
+	if deployment.Operation.Status == "DONE" {
+		doe := deployment.Operation.Error
+		if doe != nil && len(doe.Errors) > 0 {
+			errors := []DeploymentError{}
+			for _, e := range doe.Errors {
+				errors = append(errors, DeploymentError{
+					Code: e.Code,
+					Location: e.Location,
+					Message: e.Message,
+				})
+			}
+			pl.Props.Errors = errors
+			pl.Props.Status = broken
+		} else {
+			pl.Props.Status = opened
+		}
+		err = pl.update(ctx)
+		if err != nil {
+			log.Errorf(ctx, "Failed to update Pipeline Status to %v: %v\npl: %v\n", pl.Props.Status, err, pl)
+			return err
+		}
+	}
 	return nil
 }
