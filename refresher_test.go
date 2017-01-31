162fea8858fd916f9f830ee1f0eb6f6b1517b01d
akm <akm2000@gmail.com>
2017-01-31 12:01:41 +0900

    Add test about refresing on closing

--- refresher_test.go
+++ refresher_test.go
@@ -72,7 +72,7 @@ func (d *TestDeployerError) GetOperation(ctx context.Context, project string, op
 	}, nil
 }
 
-func TestRefresherProcess(t *testing.T) {
+func TestRefresherProcessForDeploying(t *testing.T) {
 	ctx, done, err := aetest.NewContext()
 	assert.NoError(t, err)
 	defer done()
@@ -130,6 +130,68 @@ func TestRefresherProcess(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, expection.status, pl2.Props.Status)
 		assert.Equal(t, expection.errors, pl2.Props.DeployingErrors)
+	}
+
+}
+
+func TestRefresherProcessForClosing(t *testing.T) {
+	ctx, done, err := aetest.NewContext()
+	assert.NoError(t, err)
+	defer done()
+
+	type Expection struct {
+		status   Status
+		deployer DeploymentServicer
+		errors   []DeploymentError
+	}
+
+	expections := []Expection{
+		Expection{
+			status:   closing,
+			deployer: &TestDeployerRunning{},
+			errors:   nil,
+		},
+		Expection{
+			status:   closed,
+			deployer: &TestDeployerOK{},
+			errors:   nil,
+		},
+		Expection{
+			status:   closing_error,
+			deployer: &TestDeployerError{},
+			errors: []DeploymentError{
+				DeploymentError{
+					Code:     "999",
+					Location: "Somewhere",
+					Message:  "Something wrong",
+				},
+			},
+		},
+	}
+
+	for _, expection := range expections {
+		plp := PipelineProps{
+			Name:           "pipeline01",
+			ProjectID:      proj,
+			Zone:           "us-central1-f",
+			SourceImage:    "https://www.googleapis.com/compute/v1/projects/google-containers/global/images/gci-stable-55-8872-76-0",
+			MachineType:    "f1-micro",
+			TargetSize:     2,
+			ContainerSize:  2,
+			ContainerName:  "groovenauts/batch_type_iot_example:0.3.1",
+			Command:        "bundle exec magellan-gcs-proxy echo %{download_files.0} %{downloads_dir} %{uploads_dir}",
+			DeploymentName: "pipeline01",
+			Status:         closing,
+		}
+		pl, err := CreatePipeline(ctx, &plp)
+
+		r := &Refresher{deployer: expection.deployer}
+		err = r.Process(ctx, pl)
+		assert.NoError(t, err)
+		pl2, err := FindPipeline(ctx, pl.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, expection.status, pl2.Props.Status)
+		assert.Equal(t, expection.errors, pl2.Props.ClosingErrors)
 	}
 
 }
