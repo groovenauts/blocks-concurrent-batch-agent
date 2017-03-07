5c82a4e0534322fbb782505867c9ceb72478d367
akm <akm2000@gmail.com>
2017-03-07 19:53:50 +0900

    Fix the logging code. Don't use the result before err checking.

--- handler.go
+++ handler.go
@@ -136,10 +136,11 @@ func (h *handler) create(c echo.Context) error {
 		return err
 	}
 	pl, err := CreatePipeline(ctx, plp)
-	log.Debugf(ctx, "Created pipeline: %v\nProps: %v\n", pl, pl.Props)
 	if err != nil {
+		log.Errorf(ctx, "Failed to create pipeline with Props: %v\n%v\n", plp, err)
 		return err
 	}
+	log.Debugf(ctx, "Created pipeline: %v\nProps: %v\n", pl, pl.Props)
 	if !plp.Dryrun {
 		t := taskqueue.NewPOSTTask("/pipelines/"+pl.ID+"/build_task", map[string][]string{})
 		t.Header.Add(AUTH_HEADER, req.Header.Get(AUTH_HEADER))
