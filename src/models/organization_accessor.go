024d47049459d5855c4d5ae1df0a6a742f9f323e
akm <akm2000@gmail.com>
2017-06-20 15:15:53 +0900

    :+1: Check id before finding by using it

--- src/models/organization_accessor.go
+++ src/models/organization_accessor.go
@@ -1,12 +1,22 @@
 package models
 
 import (
+	"fmt"
 	"errors"
+	// "runtime/debug"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 )
+
+type InvalidOrganizationID struct {
+	ID string
+}
+
+func (e *InvalidOrganizationID) Error() string {
+	return fmt.Sprintf("Invalid Organization ID: %q", e.ID)
+}
 
 type OrganizationAccessor struct {
 }
@@ -16,6 +26,13 @@ var GlobalOrganizationAccessor = &OrganizationAccessor{}
 var ErrNoSuchOrganization = errors.New("No such data in Organizations")
 
 func (aa *OrganizationAccessor) Find(ctx context.Context, id string) (*Organization, error) {
+	if id == "" {
+		// debug.PrintStack()
+		err := &InvalidOrganizationID{id}
+		log.Errorf(ctx, "OrganizationAccessor#Find %v\n", err)
+		return nil, err
+	}
+
 	key, err := datastore.DecodeKey(id)
 	if err != nil {
 		log.Errorf(ctx, "OrganizationAccessor#Find %v id: %q\n", err, id)
