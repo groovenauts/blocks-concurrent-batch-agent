6327b5597389fb8039d2f108492af69363cfdadc
akm <akm2000@gmail.com>
2017-06-30 11:50:11 +0900

    :green_heart: Increase retry count

--- src/models/pipeline_test.go
+++ src/models/pipeline_test.go
@@ -271,7 +271,7 @@ func TestGetActiveSubscriptions(t *testing.T) {
 	}
 
 	var res []*Subscription
-	test_utils.RetryWith(10, func() func(){
+	test_utils.RetryWith(12, func() func(){
 		res, err = GlobalPipelineAccessor.GetActiveSubscriptions(ctx)
 		assert.NoError(t, err)
 		if 1 == len(res) {
