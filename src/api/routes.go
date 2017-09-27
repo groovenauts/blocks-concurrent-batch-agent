1f091722a64570e57e1672920bd10ef850b83ac2
akm <akm2000@gmail.com>
2017-09-27 11:14:37 +0900

    :recycle: Inline actions into routes.go

--- src/api/routes.go
+++ src/api/routes.go
@@ -10,19 +10,18 @@ func SetupRoutes(echo *echo.Echo) map[string]interface{} {
 	e = echo
 
 	h := &PipelineHandler{}
-	h.buildActions()
 
 	g := e.Group("/orgs/:org_id/pipelines")
-	g.GET("", h.Actions["index"])
-	g.POST("", h.Actions["create"])
-	g.GET("/subscriptions", h.Actions["subscriptions"])
+	g.GET("", h.collection("org_id", h.index))
+	g.POST("", h.collection("org_id", h.create))
+	g.GET("/subscriptions", h.collection("org_id", h.subscriptions))
 
 	g = e.Group("/pipelines")
-	g.GET("/:id", h.Actions["show"])
-	g.PUT("/:id/cancel", h.Actions["cancel"])
-	g.PUT("/:id/close", h.Actions["close"])
+	g.GET("/:id", h.member("id", h.show))
+	g.PUT("/:id/cancel", h.member("id", h.cancel))
+	g.PUT("/:id/close", h.member("id", h.cancel))
 	g.POST("/:id/close_task", h.member("id", h.closeTask))
-	g.DELETE("/:id", h.Actions["destroy"])
+	g.DELETE("/:id", h.member("id", h.destroy))
 
 	g.POST("/:id/build_task", h.member("id", h.buildTask))
 	g.POST("/:id/wait_building_task", h.member("id", h.waitBuildingTask))
@@ -30,19 +29,18 @@ func SetupRoutes(echo *echo.Echo) map[string]interface{} {
 	g.POST("/:id/subscribe_task", h.member("id", h.subscribeTask))
 	g.POST("/:id/wait_closing_task", h.member("id", h.waitClosingTask))
 
-	g.POST("/:id/refresh", h.Actions["refresh"])
+	g.POST("/:id/refresh", h.member("id", h.refresh))
 	g.POST("/:id/refresh_task", h.member("id", h.refreshTask))
 
 	jh := &JobHandler{}
-	jhActions := jh.buildActions()
 
 	g = e.Group("/pipelines/:pipeline_id/jobs")
-	g.GET("", jhActions["index"])
-	g.POST("", jhActions["create"])
+	g.GET("", jh.collection("pipeline_id", jh.index))
+	g.POST("", jh.collection("pipeline_id", jh.create))
 
 	g = e.Group("/jobs")
-	g.GET("/:id", jhActions["show"])
-	g.POST("/:id/getready", jhActions["getready"])
+	g.GET("/:id", jh.member("id", h.show))
+	g.POST("/:id/getready", jh.member("id", jh.getReady))
 	g.POST("/:id/wait_task", jh.member("id", jh.WaitToPublishTask))
 	g.POST("/:id/publish_task", jh.member("id", jh.PublishTask))
 
