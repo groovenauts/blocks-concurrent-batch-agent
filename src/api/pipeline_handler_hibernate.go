9756a6bccebb01c6ad64350ecde4d4a1d8f6664d
akm <akm2000@gmail.com>
2017-11-15 16:25:35 +0900

    :+1: Implement hibernation tasks

--- src/api/pipeline_handler_hibernate.go
+++ src/api/pipeline_handler_hibernate.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -8,6 +9,7 @@ import (
 
 	"github.com/labstack/echo"
 	"golang.org/x/net/context"
+	"google.golang.org/api/googleapi"
 	"google.golang.org/appengine/log"
 )
 
@@ -32,5 +34,60 @@ func (h *PipelineHandler) checkHibernationTask(c echo.Context) error {
 		return h.ReturnJsonWith(c, pl, http.StatusCreated, func() error {
 			return h.PostPipelineTask(c, "hibernate_task", pl)
 		})
+	}
+}
+
+// curl -v -X	POST http://localhost:8080/pipelines/1/hibernate_task
+func (h *PipelineHandler) hibernateTask(c echo.Context) error {
+	ctx := c.Get("aecontext").(context.Context)
+	pl := c.Get("pipeline").(*models.Pipeline)
+	closer, err := models.NewCloser(ctx, pl.StartHibernation)
+	if err != nil {
+		log.Errorf(ctx, "Failed to create new closer because of %v\n", err)
+		return err
+	}
+	err = closer.Process(ctx, pl)
+	if err != nil {
+		switch err.(type) {
+		case *googleapi.Error:
+			e2 := err.(*googleapi.Error)
+			switch e2.Code {
+			case http.StatusNotFound: // googleapi: Error 404: The object 'projects/optical-hangar-158902/global/deployments/pipeline-mjr-89-20170926-223541' is not found., notFound
+				log.Warningf(ctx, "Skip closing because of %v", e2.Message)
+				return c.JSON(http.StatusOK, pl)
+			}
+		}
+		log.Errorf(ctx, "Failed to hibernate pipeline because of %v\n", err)
+		return err
+	}
+
+	return h.ReturnJsonWith(c, pl, http.StatusCreated, func() error {
+		return h.PostPipelineTask(c, "wait_hibernation_task", pl)
+	})
+}
+
+// curl -v -X	POST http://localhost:8080/pipelines/1/wait_hibernation_task
+func (h *PipelineHandler) waitHibernationTask(c echo.Context) error {
+	started := time.Now()
+	ctx := c.Get("aecontext").(context.Context)
+	pl := c.Get("pipeline").(*models.Pipeline)
+	handler := pl.HibernationHandler(ctx)
+
+	refresher := &models.Refresher{}
+	err := refresher.Process(ctx, pl, handler)
+	if err != nil {
+		log.Errorf(ctx, "Failed to refresh pipeline %v because of %v\n", pl, err)
+		return err
+	}
+
+	switch pl.Status {
+	case models.HibernationStarting:
+		return h.ReturnJsonWith(c, pl, http.StatusAccepted, func() error {
+			return h.PostPipelineTaskWithETA(c, "wait_hibernation_task", pl, started.Add(30*time.Second))
+		})
+	case models.Hibernating:
+		return c.JSON(http.StatusOK, pl)
+	default:
+		return &models.InvalidStateTransition{Msg: fmt.Sprintf("Unexpected Status: %v for Pipeline: %v", pl.Status, pl)}
 	}
 }
