d0ded53f8c85eaa3944050e110840d9062ff95a8
akm <akm2000@gmail.com>
2018-01-16 11:21:42 +0900

    :recycle: Extract PostTask, PostTaskWithEta, PostTaskWith

--- src/api/task_support.go
+++ src/api/task_support.go
@@ -13,10 +13,48 @@ import (
 	"google.golang.org/appengine/taskqueue"
 )
 
+// Methods For Pipeline
+
+func PipelineTaskPath(action string, pl *models.Pipeline) string {
+	return fmt.Sprintf("/pipelines/%s/%s", pl.ID, action)
+}
+
+func PostPipelineTask(c echo.Context, action string, pl *models.Pipeline) error {
+	return PostTask(c, PipelineTaskPath(action, pl))
+}
+
+func PostPipelineTaskWithETA(c echo.Context, action string, pl *models.Pipeline, eta time.Time) error {
+	return PostTaskWithETA(c, PipelineTaskPath(action, pl), eta)
+}
+
 func PostPipelineTaskWith(c echo.Context, action string, pl *models.Pipeline, params url.Values, f func(*taskqueue.Task) error) error {
+	return PostTaskWith(c, PipelineTaskPath(action, pl), params, f)
+}
+
+// Base methods for tasks
+
+func PostTask(c echo.Context, path string) error {
+	return PostTaskWithETA(c, path, time.Now())
+}
+
+func PostTaskWithETA(c echo.Context, path string, eta time.Time) error {
+	err := PostTaskWith(c, path, url.Values{}, SetETAFunc(eta))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func SetETAFunc(eta time.Time) func(t *taskqueue.Task) error {
+	return func(t *taskqueue.Task) error {
+		t.ETA = eta
+		return nil
+	}
+}
+
+func PostTaskWith(c echo.Context, path string, params url.Values, f func(*taskqueue.Task) error) error {
 	ctx := c.Get("aecontext").(context.Context)
 	req := c.Request()
-	path := fmt.Sprintf("/pipelines/%s/%s", pl.ID, action)
 	t := taskqueue.NewPOSTTask(path, params)
 	t.Header.Add(AUTH_HEADER, req.Header.Get(AUTH_HEADER))
 	if f != nil {
@@ -34,29 +72,10 @@ func PostPipelineTaskWith(c echo.Context, action string, pl *models.Pipeline, pa
 	return nil
 }
 
-func SetETAFunc(eta time.Time) func(t *taskqueue.Task) error {
-	return func(t *taskqueue.Task) error {
-		t.ETA = eta
-		return nil
-	}
-}
-
-func PostPipelineTaskWithETA(c echo.Context, action string, pl *models.Pipeline, eta time.Time) error {
-	err := PostPipelineTaskWith(c, action, pl, url.Values{}, SetETAFunc(eta))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func PostPipelineTask(c echo.Context, action string, pl *models.Pipeline) error {
-	return PostPipelineTaskWithETA(c, action, pl, time.Now())
-}
-
-func ReturnJsonWith(c echo.Context, pl *models.Pipeline, status int, f func() error) error {
+func ReturnJsonWith(c echo.Context, model interface{}, status int, f func() error) error {
 	err := f()
 	if err != nil {
 		return err
 	}
-	return c.JSON(status, pl)
+	return c.JSON(status, model)
 }
