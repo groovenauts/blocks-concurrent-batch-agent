8d45629b5181a6db0cceb9a0d8bc86372f2865c2
akm <akm2000@gmail.com>
2018-06-07 16:20:21 +0900

    :+1: Putする際にInstanceGroupのIdにuuidを割り振るようにしました
    goonのidとしてstringのフィールドを指定すると自動で割り振られないので。

--- model/instance_group.go
+++ model/instance_group.go
@@ -5,6 +5,8 @@ import (
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 
+	"github.com/goadesign/goa/uuid"
+
 	"github.com/mjibson/goon"
 )
 
@@ -41,7 +43,11 @@ type InstanceGroupStore struct{}
 func (s *InstanceGroupStore) GetAll(ctx context.Context) ([]*InstanceGroup, error) {
 	g := goon.FromContext(ctx)
 	r := []*InstanceGroup{}
-	_, err := g.GetAll(datastore.NewQuery(g.Kind(new(InstanceGroup))), &r)
+	k := g.Kind(new(InstanceGroup))
+	log.Infof(ctx, "Kind is %v\n", k)
+	q := datastore.NewQuery(k)
+	log.Infof(ctx, "q is %v\n", q)
+	_, err := g.GetAll(q.EventualConsistency(), &r)
 	if err != nil {
 		log.Errorf(ctx, "Failed to GetAll InstanceGroup because of %v\n", err)
 		return nil, err
@@ -49,8 +55,22 @@ func (s *InstanceGroupStore) GetAll(ctx context.Context) ([]*InstanceGroup, erro
 	return r, nil
 }
 
+func (s *InstanceGroupStore) Get(ctx context.Context, id string) (*InstanceGroup, error) {
+	g := goon.FromContext(ctx)
+	r := InstanceGroup{Id: id}
+	err := g.Get(&r)
+	if err != nil {
+		log.Errorf(ctx, "Failed to Get InstanceGroup because of %v\n", err)
+		return nil, err
+	}
+	return &r, nil
+}
+
 func (s *InstanceGroupStore) Put(ctx context.Context, m *InstanceGroup) (*datastore.Key, error) {
 	g := goon.FromContext(ctx)
+	if m.Id == "" {
+		m.Id = uuid.NewV4().String()
+	}
 	key, err := g.Put(m)
 	if err != nil {
 		log.Errorf(ctx, "Failed to Put %v because of %v\n", m, err)
