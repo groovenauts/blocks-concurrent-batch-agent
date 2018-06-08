2c252a43eb8ad8f380442777a5bd113ebe24d280
akm <akm2000@gmail.com>
2018-06-08 21:24:32 +0900

    :+1: POST /dummy-authsを実装しました

--- controller/dummy-auths.go
+++ controller/dummy-auths.go
@@ -1,8 +1,13 @@
 package controller
 
 import (
+	"time"
+
+	"google.golang.org/appengine"
+
 	"github.com/goadesign/goa"
 	"github.com/groovenauts/blocks-concurrent-batch-server/app"
+	"github.com/groovenauts/blocks-concurrent-batch-server/model"
 )
 
 // DummyAuthsController implements the dummy-auths resource.
@@ -20,7 +25,29 @@ func (c *DummyAuthsController) Create(ctx *app.CreateDummyAuthsContext) error {
 	// DummyAuthsController_Create: start_implement
 
 	// Put your logic here
+	appCtx := appengine.NewContext(ctx.Request)
 
-	return nil
+	t := time.Now()
+	org := &model.Organization{
+		Name: "dummy-" + t.Format(time.RFC3339),
+		TokenAmount: 100,
+	}
+	err := org.Create(appCtx)
+	if err != nil {
+		return ctx.BadRequest(goa.ErrBadRequest(err))
+	}
+
+	auth := &model.Auth{Organization: org}
+	err = auth.Create(appCtx)
+	if err != nil {
+		return ctx.BadRequest(goa.ErrBadRequest(err))
+	}
+
+	res := &app.DummyAuth{
+		OrganizationID: org.ID,
+		Token: auth.Token,
+	}
+	return ctx.Created(res)
+
 	// DummyAuthsController_Create: end_implement
 }
