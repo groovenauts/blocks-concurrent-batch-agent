21717f7bf42cdb8345d46ef4797e36281bf75db0
akm <akm2000@gmail.com>
2018-06-08 20:52:20 +0900

    :+1: design/debug.go を追加しました

---
+++ design/debug.go
@@ -0,0 +1,32 @@
+package design
+
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+
+
+
+var DummyAuth = MediaType("application/vnd.dummy-auth+json", func() {
+	Description("Dummy auth")
+
+	Attributes(func() {
+		Attribute("organization_id", String)
+		Attribute("token", String)
+	})
+	View("default", func() {
+		Attribute("organization_id")
+		Attribute("token")
+	})
+})
+
+var _ = Resource("dummy-auths", func() {
+	BasePath("/dummy-auths")
+	DefaultMedia(DummyAuth)
+	Action("create", func() {
+		Description("create")
+		Routing(POST(""))
+		Response(Created, DummyAuth)
+	})
+})
