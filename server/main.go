3ecb924c74c6dfe402a6f8bd90f731520ed1c877
akm <akm2000@gmail.com>
2018-06-08 18:20:35 +0900

    :+1: server/main.go を修正しました
    1. Change package from "main" to "server"
    2. Add "net/http" to import section
    3. Add "github.com/groovenauts/blocks-concurrent-batch-server/controller" to import section
    4. Change "func main()" to "func init()"
    5. Add "controller." before each "NewXxxxController"
    6. Comment out the lines below the comment "Start service"
    7. Add http.HandleFunc("/", service.Mux.ServeHTTP) at the end of init func

--- server/main.go
+++ server/main.go
@@ -1,14 +1,17 @@
 //go:generate goagen bootstrap -d github.com/groovenauts/blocks-concurrent-batch-server/design
 
-package main
+package server
 
 import (
+	"net/http"
+
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware"
 	"github.com/groovenauts/blocks-concurrent-batch-server/app"
+	"github.com/groovenauts/blocks-concurrent-batch-server/controller"
 )
 
-func main() {
+func init() {
 	// Create service
 	service := goa.New("appengine")
 
@@ -19,45 +22,46 @@ func main() {
 	service.Use(middleware.Recover())
 
 	// Mount "Auth" controller
-	c := NewAuthController(service)
+	c := controller.NewAuthController(service)
 	app.MountAuthController(service, c)
 	// Mount "InstanceGroupConstructingTask" controller
-	c2 := NewInstanceGroupConstructingTaskController(service)
+	c2 := controller.NewInstanceGroupConstructingTaskController(service)
 	app.MountInstanceGroupConstructingTaskController(service, c2)
 	// Mount "InstanceGroupDestructingTask" controller
-	c3 := NewInstanceGroupDestructingTaskController(service)
+	c3 := controller.NewInstanceGroupDestructingTaskController(service)
 	app.MountInstanceGroupDestructingTaskController(service, c3)
 	// Mount "InstanceGroupResizingTask" controller
-	c4 := NewInstanceGroupResizingTaskController(service)
+	c4 := controller.NewInstanceGroupResizingTaskController(service)
 	app.MountInstanceGroupResizingTaskController(service, c4)
 	// Mount "IntanceGroup" controller
-	c5 := NewIntanceGroupController(service)
+	c5 := controller.NewIntanceGroupController(service)
 	app.MountIntanceGroupController(service, c5)
 	// Mount "Job" controller
-	c6 := NewJobController(service)
+	c6 := controller.NewJobController(service)
 	app.MountJobController(service, c6)
 	// Mount "Organization" controller
-	c7 := NewOrganizationController(service)
+	c7 := controller.NewOrganizationController(service)
 	app.MountOrganizationController(service, c7)
 	// Mount "Pipeline" controller
-	c8 := NewPipelineController(service)
+	c8 := controller.NewPipelineController(service)
 	app.MountPipelineController(service, c8)
 	// Mount "PipelineBase" controller
-	c9 := NewPipelineBaseController(service)
+	c9 := controller.NewPipelineBaseController(service)
 	app.MountPipelineBaseController(service, c9)
 	// Mount "PipelineBaseClosingTask" controller
-	c10 := NewPipelineBaseClosingTaskController(service)
+	c10 := controller.NewPipelineBaseClosingTaskController(service)
 	app.MountPipelineBaseClosingTaskController(service, c10)
 	// Mount "PipelineBaseOpeningTask" controller
-	c11 := NewPipelineBaseOpeningTaskController(service)
+	c11 := controller.NewPipelineBaseOpeningTaskController(service)
 	app.MountPipelineBaseOpeningTaskController(service, c11)
 	// Mount "swagger" controller
-	c12 := NewSwaggerController(service)
+	c12 := controller.NewSwaggerController(service)
 	app.MountSwaggerController(service, c12)
 
-	// Start service
-	if err := service.ListenAndServe(":8080"); err != nil {
-		service.LogError("startup", "err", err)
-	}
+	// // Start service
+	// if err := service.ListenAndServe(":8080"); err != nil {
+	// 	service.LogError("startup", "err", err)
+	// }
 
+	http.HandleFunc("/", service.Mux.ServeHTTP)
 }
