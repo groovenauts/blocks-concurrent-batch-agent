f61b2320c2d4605646759bd27a82d07649d76862
akm <akm2000@gmail.com>
2018-06-08 18:17:11 +0900

    :+1: /pipelinesのリソースを追加しました

--- design/pipeline.go
+++ design/pipeline.go
@@ -44,3 +44,79 @@ var Pipeline = MediaType("application/vnd.pipeline+json", func() {
 		UseTrait(TimestampsViewTrait)
 	})
 })
+
+var _ = Resource("Pipeline", func() {
+	BasePath("/pipelines")
+	DefaultMedia(Pipeline)
+	Action("list", func() {
+		Description("list")
+		Routing(GET(""))
+		Params(func() {
+			Param("org_id", String, "Organization ID")
+			Required("org_id")
+		})
+		Response(OK, CollectionOf(Pipeline))
+		UseTrait(DefineTrait)
+	})
+	Action("create", func() {
+		Description("create")
+		Routing(POST(""))
+		Params(func() {
+			Param("org_id", String, "Organization ID")
+			Required("org_id")
+		})
+		Payload(PipelinePayload)
+		Response(Created, Pipeline)
+		UseTrait(DefineTrait)
+	})
+	Action("show", func() {
+		Description("show")
+		Routing(GET("/:id"))
+		Params(func() {
+			Param("id")
+		})
+		Response(OK, Pipeline)
+		UseTrait(DefineTrait)
+	})
+	Action("preparing_finalize_task", func() {
+		Description("Task to finalize current_preparing or next_preparing status")
+		Routing(PUT("/:id/preparing_finalize_task"))
+		Params(func() {
+			Param("id")
+			Param("operation_id")
+			Param("error")
+		})
+		Response(OK, Pipeline)
+		UseTrait(DefineTrait)
+	})
+	Action("current", func() {
+		Description("Update current pipeline base")
+		Routing(PUT("/:id/current"))
+		Params(func() {
+			Param("id")
+			Param("pipeline_base_id")
+			Required("pipeline_base_id")
+		})
+		Response(OK, Pipeline)
+		UseTrait(DefineTrait)
+	})
+	Action("stop", func() {
+		Description("Stop pipeline")
+		Routing(PUT("/:id/stop"))
+		Params(func() {
+			Param("id")
+		})
+		Response(OK, Pipeline)
+		UseTrait(DefineTrait)
+	})
+	Action("delete", func() {
+		Description("delete")
+		Routing(DELETE("/:id"))
+		Params(func() {
+			Param("id")
+			Required("id")
+		})
+		Response(OK, Pipeline)
+		UseTrait(DefineTrait)
+	})
+})
