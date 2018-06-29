a6616666c16bba64a3b6f59e3a8f4922c75cd807
akm <akm2000@gmail.com>
2018-06-29 19:34:17 +0900

    :+1: Add JobOutput

--- design/job.go
+++ design/job.go
@@ -38,7 +38,6 @@ var Job = MediaType("application/vnd.job+json", func() {
 		Attribute("pipeline_base_id", String, "PipelineBase ID (UUID)")
 		Attribute("message_id", String, "Pubsub Message ID")
 		Attribute("host_name", String, "Hostname where job is running")
-		// Attribute("output", String, "Job output")
 		Attribute("publish_time", DateTime, "Time when job is published")
 		Attribute("start_time", DateTime, "Time when job starts")
 		Attribute("finish_time", DateTime, "Time when job finishes")
@@ -54,12 +53,27 @@ var Job = MediaType("application/vnd.job+json", func() {
 		}
 		outputAttrs := []string{
 			"status", "pipeline_id", "pipeline_base_id", "message_id",
-			"host_name", "publish_time", "start_time", "finish_time",
+			"host_name", // "output",
+			"publish_time", "start_time", "finish_time",
 		}
 		for _, attrName := range outputAttrs {
 			Attribute(attrName)
 		}
 		UseTrait(TimestampsViewTrait)
+	})
+})
+
+var JobOutput = MediaType("application/vnd.job-output+json", func() {
+	Description("job output")
+
+	Attributes(func() {
+		UseTrait(IdTrait)
+		Attribute("output", String, "Job output")
+		Required("id", "output")
+	})
+	View("default", func() {
+		Attribute("id")
+		Attribute("output")
 	})
 })
 
@@ -87,6 +101,15 @@ var _ = Resource("Job", func() {
 			Param("id")
 		})
 		Response(OK, Job)
+		UseTrait(DefaultResponseTrait)
+	})
+	Action("output", func() {
+		Description("output")
+		Routing(GET("/:id/output"))
+		Params(func() {
+			Param("id")
+		})
+		Response(OK, JobOutput)
 		UseTrait(DefaultResponseTrait)
 	})
 	Action("activate", func() {
