fe31b2a683206ff6391677e93071ae293adee7f6
akm <akm2000@gmail.com>
2018-07-01 17:02:29 +0900

    :shower: Remove converters for JobMessage
    $ make converter_gen

--- controller/job_conv.go
+++ controller/job_conv.go
@@ -5,28 +5,6 @@ import (
 	"github.com/groovenauts/blocks-concurrent-batch-server/model"
 )
 
-func JobMessagePayloadToModel(src *app.JobMessage) model.JobMessage {
-	if src == nil {
-		return model.JobMessage{}
-	}
-	return model.JobMessage{
-		Data: StringPointerToString(src.Data),
-		// AttributeEntries no payload field
-		// No model field for payload field "attributes"
-	}
-}
-
-func JobMessageModelToMediaType(src *model.JobMessage) *app.JobMessage {
-	if src == nil {
-		return nil
-	}
-	return &app.JobMessage{
-		Data: &src.Data,
-		// AttributeEntries no media type field
-		// No field for media type field "attributes"
-	}
-}
-
 func JobPayloadToModel(src *app.JobPayload) model.Job {
 	if src == nil {
 		return model.Job{}
