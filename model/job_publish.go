ee854715e5a87cb6226e1f1b6fbbad0870523cc2
akm <akm2000@gmail.com>
2018-07-06 16:46:15 +0900

    :+1: Implement Job.Publish

--- model/job_publish.go
+++ model/job_publish.go
@@ -1,15 +1,25 @@
 package model
 
 import (
+	"encoding/base64"
 	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/log"
+
+	pubsub "google.golang.org/api/pubsub/v1"
 )
 
-func (job *Job) Publish(ctx context.Context) error {
+func (m *Job) Publish(ctx context.Context) error {
 	msg := m.BuildMessage()
-	topic := m.PipelineBase.JobTopicFqn()
+
+	pbStore := &PipelineBaseStore{}
+	pb, err := pbStore.Get(ctx, m.Parent.StringID())
+	if err != nil {
+		return err
+	}
+
+	topic := pb.JobTopicFqn()
 	log.Debugf(ctx, "Job.Publish msg %v to topic %q\n", msg, topic)
 
 	req := &pubsub.PublishRequest{
@@ -28,5 +38,23 @@ func (job *Job) Publish(ctx context.Context) error {
 	return nil
 }
 
-func (job *Job) BuildMessage(ctx context.Context) error {
+const JobIdKey = "concurrent_batch.job_id"
+
+func (m *Job) BuildMessage() *pubsub.PubsubMessage {
+	entry := JobKeyValuePair{Name: JobIdKey, Value: m.Id}
+	m.Message.AttributeEntries = append(m.Message.AttributeEntries, entry)
+	return &pubsub.PubsubMessage{
+		Attributes: m.Message.AttributeEntries.Map(),
+		Data:       base64.StdEncoding.EncodeToString([]byte(m.Message.Data)),
+	}
+}
+
+
+
+func (pairs JobKeyValuePairs) Map() map[string]string {
+	r := map[string]string{}
+	for _, entry := range pairs {
+		r[entry.Name] = entry.Value
+	}
+	return r
 }
