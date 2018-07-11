e1007a718bff037a3f56bd5d4621e96e85b08fb5
akm <akm2000@gmail.com>
2018-07-11 18:16:52 +0900

    :+1: Add debug loggings

--- src/api/operation_handler.go
+++ src/api/operation_handler.go
@@ -32,14 +32,18 @@ func (h *OperationHandler) waitBuildingTask(c echo.Context) error {
 	started := time.Now()
 	ctx := c.Get("aecontext").(context.Context)
 	operation := c.Get("operation").(*models.PipelineOperation)
+	log.Debugf(ctx, "waitBuildingTask #0 operation %v\n", operation)
 
 	err := models.WithDefaultDeploymentServicer(ctx, func(servicer models.DeploymentServicer) error {
 		updater := &models.DeploymentUpdater{Servicer: servicer}
 		return operation.ProcessDeploy(ctx, updater)
 	})
 	if err != nil {
+		log.Errorf(ctx, "waitBuildingTask #1 operation %v\n", operation)
 		return err
 	}
+
+	log.Debugf(ctx, "waitBuildingTask #2\n")
 
 	if !operation.Done() {
 		return ReturnJsonWith(c, operation, http.StatusAccepted, func() error {
@@ -47,16 +51,21 @@ func (h *OperationHandler) waitBuildingTask(c echo.Context) error {
 		})
 	}
 
+	log.Debugf(ctx, "waitBuildingTask #3\n")
+
 	pl, err := operation.LoadPipeline(ctx)
 	if err != nil {
 		return err
 	}
+
+	log.Debugf(ctx, "waitBuildingTask #4\n")
 	if pl.Status != models.Opened {
 		return &models.InvalidStateTransition{
 			Msg: fmt.Sprintf("Unexpected Status: %v for Pipeline: %v", pl.Status, pl),
 		}
 	}
 
+	log.Debugf(ctx, "waitBuildingTask #5\n")
 	if pl.Cancelled {
 		return ReturnJsonWith(c, pl, http.StatusNoContent, func() error {
 			return PostPipelineTask(c, "close_task", pl)
@@ -73,58 +82,72 @@ func (h *OperationHandler) waitHibernationTask(c echo.Context) error {
 	started := time.Now()
 	ctx := c.Get("aecontext").(context.Context)
 	operation := c.Get("operation").(*models.PipelineOperation)
+	log.Debugf(ctx, "waitHibernationTask #0 operation: %v\n", operation)
 
 	err := models.WithDefaultDeploymentServicer(ctx, func(servicer models.DeploymentServicer) error {
 		updater := &models.DeploymentUpdater{Servicer: servicer}
 		return operation.ProcessHibernation(ctx, updater)
 	})
 	if err != nil {
+		log.Errorf(ctx, "waitHibernationTask #1 operation: %v\n", operation)
 		return err
 	}
 
+	log.Debugf(ctx, "waitHibernationTask #2\n")
 	if !operation.Done() {
 		return ReturnJsonWith(c, operation, http.StatusAccepted, func() error {
 			return PostOperationTaskWithETA(c, "wait_hibernation_task", operation, started.Add(30*time.Second))
 		})
 	}
 
+	log.Debugf(ctx, "waitHibernationTask #3\n")
 	pl, err := operation.LoadPipeline(ctx)
 	if err != nil {
 		return err
 	}
 
+	log.Debugf(ctx, "waitHibernationTask #4\n")
 	switch pl.Status {
 	case models.Hibernating:
 		if pl.Cancelled {
+			log.Debugf(ctx, "waitHibernationTask #5\n")
 			err := pl.CloseIfHibernating(ctx)
 			if err != nil {
 				log.Errorf(ctx, "Failed to CloseAfterHibernation because of %v\n", err)
 				return err
 			}
+			log.Debugf(ctx, "waitHibernationTask #6\n")
 			return c.JSON(http.StatusOK, pl)
 		}
 
+		log.Debugf(ctx, "waitHibernationTask #7\n")
 		newTask, err := pl.HasNewTaskSince(ctx, pl.HibernationStartedAt)
 		if err != nil {
 			log.Errorf(ctx, "Failed to check new tasks because of %v\n", err)
 			return err
 		}
+		log.Debugf(ctx, "waitHibernationTask #8\n")
 		if newTask {
+			log.Debugf(ctx, "waitHibernationTask #9\n")
 			err := pl.BackToBeReserved(ctx)
 			if err != nil {
 				log.Errorf(ctx, "Failed to BackToReady because of %v\n", err)
 				return err
 			}
+			log.Debugf(ctx, "waitHibernationTask #10\n")
 			return ReturnJsonWith(c, pl, http.StatusCreated, func() error {
 				return PostPipelineTask(c, "build_task", pl)
 			})
 		} else {
+			log.Debugf(ctx, "waitHibernationTask #11\n")
 			return c.JSON(http.StatusOK, pl)
 		}
 	case models.HibernationError:
+		log.Debugf(ctx, "waitHibernationTask #12\n")
 		log.Infof(ctx, "Pipeline is already %v so quit wait_hibernation_task\n", pl.Status)
 		return c.JSON(http.StatusOK, pl)
 	default:
+		log.Debugf(ctx, "waitHibernationTask #13\n")
 		return &models.InvalidStateTransition{
 			Msg: fmt.Sprintf("Unexpected Status: %v for Pipeline: %v", pl.Status, pl),
 		}
@@ -136,6 +159,7 @@ func (h *OperationHandler) waitClosingTask(c echo.Context) error {
 	started := time.Now()
 	ctx := c.Get("aecontext").(context.Context)
 	operation := c.Get("operation").(*models.PipelineOperation)
+	log.Debugf(ctx, "waitClosingTask #0 operation: %v\n", operation)
 
 	err := models.WithDefaultDeploymentServicer(ctx, func(servicer models.DeploymentServicer) error {
 		updater := &models.DeploymentUpdater{Servicer: servicer}
@@ -144,15 +168,18 @@ func (h *OperationHandler) waitClosingTask(c echo.Context) error {
 		})
 	})
 	if err != nil {
+		log.Errorf(ctx, "waitClosingTask #1 operation: %v\n", operation)
 		return err
 	}
 
+	log.Debugf(ctx, "waitClosingTask #2\n")
 	if !operation.Done() {
 		return ReturnJsonWith(c, operation, http.StatusAccepted, func() error {
 			return PostOperationTaskWithETA(c, "wait_closing_task", operation, started.Add(30*time.Second))
 		})
 	}
 
+	log.Debugf(ctx, "waitClosingTask #3\n")
 	pl, err := operation.LoadPipeline(ctx)
 	if err != nil {
 		return err
@@ -160,8 +187,10 @@ func (h *OperationHandler) waitClosingTask(c echo.Context) error {
 
 	switch pl.Status {
 	case models.Closed:
+		log.Debugf(ctx, "waitClosingTask #4\n")
 		return c.JSON(http.StatusOK, pl)
 	default:
+		log.Debugf(ctx, "waitClosingTask #5\n")
 		return &models.InvalidStateTransition{
 			Msg: fmt.Sprintf("Unexpected Status: %v for Pipeline: %v", pl.Status, pl),
 		}
@@ -173,8 +202,10 @@ func (h *OperationHandler) waitScalingTask(c echo.Context) error {
 	started := time.Now()
 	ctx := c.Get("aecontext").(context.Context)
 	operation := c.Get("operation").(*models.PipelineOperation)
+	log.Debugf(ctx, "waitScalingTask #0 operation: %v\n", operation)
 
 	return models.WithInstanceGroupServicer(ctx, func(servicer models.InstanceGroupServicer) error {
+		log.Debugf(ctx, "waitScalingTask #1\n")
 		handler_called := false
 		handler := func(_ string) error {
 			handler_called = true
@@ -197,8 +228,10 @@ func (h *OperationHandler) waitScalingTask(c echo.Context) error {
 			return err
 		}
 		if handler_called {
+			log.Debugf(ctx, "waitScalingTask #3\n")
 			return c.JSON(http.StatusOK, operation)
 		}
+		log.Debugf(ctx, "waitScalingTask #4\n")
 		return ReturnJsonWith(c, operation, http.StatusAccepted, func() error {
 			return PostOperationTaskWithETA(c, "wait_scaling_task", operation, started.Add(30*time.Second))
 		})
--- src/models/pipeline_operation.go
+++ src/models/pipeline_operation.go
@@ -6,7 +6,7 @@ import (
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
-	// "google.golang.org/appengine/log"
+	"google.golang.org/appengine/log"
 
 	"gopkg.in/go-playground/validator.v9"
 )
@@ -82,6 +82,7 @@ func (m *PipelineOperation) Update(ctx context.Context) error {
 	m.UpdatedAt = time.Now()
 	key, err := datastore.DecodeKey(m.ID)
 	if err != nil {
+		log.Errorf(ctx, "Failed to datastore.DecodeKey(%q)\n", m.ID)
 		return err
 	}
 	_, err = m.ValidateAndPut(ctx, key)
@@ -94,11 +95,13 @@ func (m *PipelineOperation) Update(ctx context.Context) error {
 func (m *PipelineOperation) ValidateAndPut(ctx context.Context, key *datastore.Key) (*datastore.Key, error) {
 	err := m.Validate()
 	if err != nil {
+		log.Errorf(ctx, "Failed to Validate PipelineOperation %v\n", m)
 		return nil, err
 	}
 
 	key, err = datastore.Put(ctx, key, m)
 	if err != nil {
+		log.Errorf(ctx, "Failed to datastore.Put(ctx, %v, %v)\n", key, m)
 		return nil, err
 	}
 	return key, nil
@@ -167,6 +170,7 @@ func (m *PipelineOperation) HasError() bool {
 func (m *PipelineOperation) LoadPipeline(ctx context.Context) (*Pipeline, error) {
 	key, err := datastore.DecodeKey(m.ID)
 	if err != nil {
+		log.Errorf(ctx, "Failed to datastore.DecodeKey(%q)\n", m.ID)
 		return nil, err
 	}
 	parentKey := key.Parent()
