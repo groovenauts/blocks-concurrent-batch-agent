8ff21078cdf9aac3ca911cfbd698e4ccda9f98be
akm <akm2000@gmail.com>
2018-07-12 13:06:23 +0900

    :+1: Fix auth_methods.go

--- model/auth_methods.go
+++ model/auth_methods.go
@@ -12,19 +12,6 @@ import (
 	"gopkg.in/go-playground/validator.v9"
 )
 
-type (
-	Auth struct {
-		ID                string        `datastore:"-"`
-		Organization      *Organization `datastore:"-" validate:"required"`
-		Token             string        `datastore:"-"`
-		Password          string        `datastore:"-"`
-		EncryptedPassword string
-		Disabled          bool
-		CreatedAt         time.Time
-		UpdatedAt         time.Time
-	}
-)
-
 func (m *Auth) Validate() error {
 	validator := validator.New()
 	err := validator.Struct(m)
@@ -32,80 +19,34 @@ func (m *Auth) Validate() error {
 }
 
 func (m *Auth) Create(ctx context.Context) error {
-	t := time.Now()
-	if m.CreatedAt.IsZero() {
-		m.CreatedAt = t
-	}
-	if m.UpdatedAt.IsZero() {
-		m.UpdatedAt = t
-	}
-
 	err := m.Validate()
 	if err != nil {
 		return err
 	}
-	m.generatePassword()
+	m.Password = m.generatePassword()
 
-	orgKey, err := datastore.DecodeKey(m.Organization.ID)
-	if err != nil {
-		return err
-	}
-	key := datastore.NewIncompleteKey(ctx, "Auths", orgKey)
-	// Password is a string encoded by base64
 	enc_pw, err := bcrypt.GenerateFromPassword([]byte(m.Password), 10)
 	if err != nil {
 		log.Errorf(ctx, "@CreateAuth %v\n", err)
 		return err
 	}
 	m.EncryptedPassword = string(enc_pw) // EncryptedPassword is binary string
-	res, err := datastore.Put(ctx, key, m)
+
+	store := &AuthStore{ParentKey: m.Parent}
+	key, err := store.Create(ctx, m)
 	if err != nil {
-		log.Errorf(ctx, "@CreateAuth %v mp: %v\n", err, m)
+		log.Errorf(ctx, "Failed to Create %v because of %v\n", m, err)
 		return err
 	}
 	// log.Debugf(ctx, "CreateAuth res: %v\n", res)
-	id := res.Encode()
-	m.ID = id
+	id := key.Encode()
 	m.Token = id + ":" + m.Password
 	// log.Debugf(ctx, "CreateAuth result: %v\n", m)
 	return nil
 }
 
-func (m *Auth) Destroy(ctx context.Context) error {
-	key, err := datastore.DecodeKey(m.ID)
-	if err != nil {
-		return err
-	}
-	if err := datastore.Delete(ctx, key); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (m *Auth) Update(ctx context.Context) error {
-	m.UpdatedAt = time.Now()
-
-	key, err := datastore.DecodeKey(m.ID)
-	if err != nil {
-		return err
-	}
-	_, err = datastore.Put(ctx, key, m)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (m *Auth) generatePassword() {
+func (m *Auth) generatePassword() string {
 	b := make([]byte, 12)
 	rand.Read(b)
-	m.Password = base64.StdEncoding.EncodeToString(b)
-}
-
-func (m *Auth) OrganizationKey() (*datastore.Key, error) {
-	key, err := datastore.DecodeKey(m.ID)
-	if err != nil {
-		return nil, err
-	}
-	return key.Parent(), nil
+	base64.StdEncoding.EncodeToString(b)
 }
