0d4eaddd0bfd810df68b55ab11ab04e6e881da02
akm <akm2000@gmail.com>
2018-07-20 19:22:56 +0900

    :+1: Expand transaction

--- controller/instance_group_task_base.go
+++ controller/instance_group_task_base.go
@@ -58,38 +58,38 @@ func (t *InstanceGroupTaskBase) WithInstanceGroupStore(ctx context.Context, orgI
 // Start
 func (t *InstanceGroupTaskBase) Start(appCtx context.Context, orgId, name string) error {
 	return t.WithInstanceGroupStore(appCtx, orgId, name, func(igStore *model.InstanceGroupStore, opeStore *model.InstanceGroupOperationStore) error {
-		m, err := igStore.ByID(appCtx, name)
-		if err != nil {
-			if err == datastore.ErrNoSuchEntity {
-				log.Errorf(appCtx, "InstanceGroup not found for %q\n", name)
-				return t.RespondNoContent(nil)
-			} else {
+		return datastore.RunInTransaction(appCtx, func(appCtx context.Context) error {
+			m, err := igStore.ByID(appCtx, name)
+			if err != nil {
+				if err == datastore.ErrNoSuchEntity {
+					log.Errorf(appCtx, "InstanceGroup not found for %q\n", name)
+					return t.RespondNoContent(nil)
+				} else {
+					return err
+				}
+			}
+
+			if m.Status != t.MainStatus {
+				if t.IsSkipped(m.Status) {
+					log.Infof(appCtx, "SKIPPING because InstanceGroup %s is already %v\n", m.Name, m.Status)
+					return t.RespondOK(nil)
+				} else {
+					log.Warningf(appCtx, "Invalid request because InstanceGroup %s is already %v\n", m.Name, m.Status)
+					return t.RespondNoContent(nil)
+				}
+			}
+
+			processor, err := t.ProcessorFactory(appCtx)
+			ope, err := processor.Process(appCtx, m)
+			if err != nil {
 				return err
 			}
-		}
 
-		if m.Status != t.MainStatus {
-			if t.IsSkipped(m.Status) {
-				log.Infof(appCtx, "SKIPPING because InstanceGroup %s is already %v\n", m.Name, m.Status)
-				return t.RespondOK(nil)
-			} else {
-				log.Warningf(appCtx, "Invalid request because InstanceGroup %s is already %v\n", m.Name, m.Status)
-				return t.RespondNoContent(nil)
+			_, err = opeStore.Put(appCtx, ope)
+			if err != nil {
+				return err
 			}
-		}
 
-		processor, err := t.ProcessorFactory(appCtx)
-		ope, err := processor.Process(appCtx, m)
-		if err != nil {
-			return err
-		}
-
-		_, err = opeStore.Put(appCtx, ope)
-		if err != nil {
-			return err
-		}
-
-		return datastore.RunInTransaction(appCtx, func(c context.Context) error {
 			m.Status = t.NextStatus
 			_, err = igStore.Put(c, m)
 			if err != nil {
@@ -107,87 +107,87 @@ func (t *InstanceGroupTaskBase) Start(appCtx context.Context, orgId, name string
 // Watch
 func (t *InstanceGroupTaskBase) Watch(appCtx context.Context, orgId, name, opeIdString string) error {
 	return t.WithInstanceGroupStore(appCtx, orgId, name, func(igStore *model.InstanceGroupStore, opeStore *model.InstanceGroupOperationStore) error {
-		opeId, err := strconv.ParseInt(opeIdString, 10, 64)
-		if err != nil {
-			log.Errorf(appCtx, "Invalid operation ID: %q\n", opeIdString)
-			return t.RespondNoContent(nil)
-		}
-
-		ope, err := opeStore.ByID(appCtx, opeId)
-		if err != nil {
-			if err == datastore.ErrNoSuchEntity {
-				log.Errorf(appCtx, "InstanceGroupOperation not found for %q\n", opeId)
-				return t.RespondNoContent(nil)
-			} else {
-				return err
-			}
-		}
-		m, err := igStore.ByKey(appCtx, ope.ParentKey)
-		if err != nil {
-			if err == datastore.ErrNoSuchEntity {
-				log.Errorf(appCtx, "InstanceGroup not found for %q\n", ope.Id)
-				return t.RespondNoContent(nil)
-			} else {
-				return err
-			}
-		}
-
-		if m.Status != t.MainStatus {
-			if t.IsSkipped(m.Status) {
-				log.Infof(appCtx, "SKIPPING because InstanceGroup %s is already %v\n", m.Name, m.Status)
-				return t.RespondOK(nil)
-			} else {
-				log.Warningf(appCtx, "Invalid request because InstanceGroup %s is already %v\n", m.Name, m.Status)
+		return datastore.RunInTransaction(appCtx, func(c context.Context) error {
+			opeId, err := strconv.ParseInt(opeIdString, 10, 64)
+			if err != nil {
+				log.Errorf(appCtx, "Invalid operation ID: %q\n", opeIdString)
 				return t.RespondNoContent(nil)
 			}
-		}
 
-		remoteOpe, err := t.RemoteOpeFunc(appCtx, ope)
-		if err != nil {
-			return err
-		}
-
-		if ope.Status != remoteOpe.Status() {
-			ope.AppendLog(fmt.Sprintf("InstanceGroup %q Status changed from %q to %q", m.Name, ope.Status, remoteOpe.Status()))
-		}
-
-		// PENDING, RUNNING, or DONE
-		switch remoteOpe.Status() {
-		case "DONE": // through
-		default:
-			if ope.Status != remoteOpe.Status() {
-				ope.Status = remoteOpe.Status()
-				_, err := opeStore.Update(appCtx, ope)
-				if err != nil {
+			ope, err := opeStore.ByID(appCtx, opeId)
+			if err != nil {
+				if err == datastore.ErrNoSuchEntity {
+					log.Errorf(appCtx, "InstanceGroupOperation not found for %q\n", opeId)
+					return t.RespondNoContent(nil)
+				} else {
 					return err
 				}
 			}
-			path := t.WatchTaskPathFunc(ope)
-			if err := PutTask(appCtx, path, 1*time.Minute); err != nil {
+			m, err := igStore.ByKey(appCtx, ope.ParentKey)
+			if err != nil {
+				if err == datastore.ErrNoSuchEntity {
+					log.Errorf(appCtx, "InstanceGroup not found for %q\n", ope.Id)
+					return t.RespondNoContent(nil)
+				} else {
+					return err
+				}
+			}
+
+			if m.Status != t.MainStatus {
+				if t.IsSkipped(m.Status) {
+					log.Infof(appCtx, "SKIPPING because InstanceGroup %s is already %v\n", m.Name, m.Status)
+					return t.RespondOK(nil)
+				} else {
+					log.Warningf(appCtx, "Invalid request because InstanceGroup %s is already %v\n", m.Name, m.Status)
+					return t.RespondNoContent(nil)
+				}
+			}
+
+			remoteOpe, err := t.RemoteOpeFunc(appCtx, ope)
+			if err != nil {
 				return err
 			}
-			return t.RespondCreated(InstanceGroupOperationModelToMediaType(ope))
-		}
 
-		errors := remoteOpe.Errors()
-		var f func(r *app.CloudAsyncOperation) error
-		if errors != nil {
-			ope.Errors = *errors
-			ope.AppendLog(fmt.Sprintf("Error by %v", remoteOpe.GetOriginal()))
-			m.Status = t.ErrorStatus
-			f = t.RespondNoContent
-		} else {
-			ope.AppendLog("Success")
-			m.Status = t.NextStatus
-			f = t.RespondAccepted
-		}
+			if ope.Status != remoteOpe.Status() {
+				ope.AppendLog(fmt.Sprintf("InstanceGroup %q Status changed from %q to %q", m.Name, ope.Status, remoteOpe.Status()))
+			}
 
-		_, err = opeStore.Update(appCtx, ope)
-		if err != nil {
-			return err
-		}
+			// PENDING, RUNNING, or DONE
+			switch remoteOpe.Status() {
+			case "DONE": // through
+			default:
+				if ope.Status != remoteOpe.Status() {
+					ope.Status = remoteOpe.Status()
+					_, err := opeStore.Update(appCtx, ope)
+					if err != nil {
+						return err
+					}
+				}
+				path := t.WatchTaskPathFunc(ope)
+				if err := PutTask(appCtx, path, 1*time.Minute); err != nil {
+					return err
+				}
+				return t.RespondCreated(InstanceGroupOperationModelToMediaType(ope))
+			}
 
-		return datastore.RunInTransaction(appCtx, func(c context.Context) error {
+			errors := remoteOpe.Errors()
+			var f func(r *app.CloudAsyncOperation) error
+			if errors != nil {
+				ope.Errors = *errors
+				ope.AppendLog(fmt.Sprintf("Error by %v", remoteOpe.GetOriginal()))
+				m.Status = t.ErrorStatus
+				f = t.RespondNoContent
+			} else {
+				ope.AppendLog("Success")
+				m.Status = t.NextStatus
+				f = t.RespondAccepted
+			}
+
+			_, err = opeStore.Update(appCtx, ope)
+			if err != nil {
+				return err
+			}
+
 			_, err = igStore.Update(c, m)
 			if err != nil {
 				return err
