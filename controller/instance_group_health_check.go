992ed53150ee9d796d5d8127717e00254a668a29
akm <akm2000@gmail.com>
2018-07-20 11:15:51 +0900

    :+1: Consider that ig has no managed instance

--- controller/instance_group_health_check.go
+++ controller/instance_group_health_check.go
@@ -122,56 +122,57 @@ func (c *InstanceGroupHealthCheckController) Execute(ctx *app.ExecuteInstanceGro
 		log.Debugf(appCtx, "Managed Instances\n", instancesJson)
 		hc.LastResult = instancesJson
 
-		if len(resp.ManagedInstances) == 0 {
-
-		}
-
-		total := 0
-		counts := map[string]int{}
-		for _, instance := range resp.ManagedInstances {
-			total += 1
-			counts[instance.InstanceStatus] += 1
-		}
-
-		// Instance status transition
-		// See https://cloud.google.com/compute/docs/instances/checking-instance-status
-		//
-		//   (START) ==> PROVISIONING ==> STAGING ==> RUNNING ==> STOPPING ==> TERMINATED ==> (END)
-		//                    ^                                                    |
-		//                    |                                                    |
-		//                    |----------------------------------------------------|
-		//
-		// ? How about these status ?
-		//   STOPPED
-		//   SUSPENDED
-		//   SUSPENDING
-		working := counts["PROVISIONING"] + counts["STAGING"] + counts["RUNNING"]
-		workingRate := int(math.Ceil(float64(working) / float64(total)))
-
-		if ig.Status == model.HealthCheckError {
-			if (working >= ig.HealthCheck.MinimumRunningSize) && (workingRate >= ig.HealthCheck.MinimumRunningPercentage) {
-				log.Infof(appCtx, "Working Instances %d is less than MinimumRunningSize %d\n", working, ig.HealthCheck.MinimumRunningSize)
-				ig.Status = model.Constructed
-			}
-		} else {
-			if working < ig.HealthCheck.MinimumRunningSize {
-				log.Warningf(appCtx, "Working Instances %d is less than MinimumRunningSize %d\n", working, ig.HealthCheck.MinimumRunningSize)
-				ig.Status = model.HealthCheckError
-			}
-			if workingRate < ig.HealthCheck.MinimumRunningPercentage {
-				log.Warningf(appCtx, "Working Instance percentages %d is less than MinimumRunningPercentage %d\n", workingRate, ig.HealthCheck.MinimumRunningPercentage)
-				ig.Status = model.HealthCheckError
-			}
-		}
 		var response func(r *app.InstanceGroupHealthCheck) error
-		if ig.Status == model.HealthCheckError {
-			response = ctx.Accepted
+
+		total := len(resp.ManagedInstances)
+		if total > 0 {
+			counts := map[string]int{}
+			for _, instance := range resp.ManagedInstances {
+				counts[instance.InstanceStatus] += 1
+			}
+
+			// Instance status transition
+			// See https://cloud.google.com/compute/docs/instances/checking-instance-status
+			//
+			//   (START) ==> PROVISIONING ==> STAGING ==> RUNNING ==> STOPPING ==> TERMINATED ==> (END)
+			//                    ^                                                    |
+			//                    |                                                    |
+			//                    |----------------------------------------------------|
+			//
+			// ? How about these statuses ?
+			//   STOPPED
+			//   SUSPENDED
+			//   SUSPENDING
+			working := counts["PROVISIONING"] + counts["STAGING"] + counts["RUNNING"]
+			workingRate := int(math.Ceil(float64(working) / float64(total)))
+
+			if ig.Status == model.HealthCheckError {
+				if (working >= ig.HealthCheck.MinimumRunningSize) && (workingRate >= ig.HealthCheck.MinimumRunningPercentage) {
+					log.Infof(appCtx, "Working Instances %d is less than MinimumRunningSize %d\n", working, ig.HealthCheck.MinimumRunningSize)
+					ig.Status = model.Constructed
+				}
+			} else {
+				if working < ig.HealthCheck.MinimumRunningSize {
+					log.Warningf(appCtx, "Working Instances %d is less than MinimumRunningSize %d\n", working, ig.HealthCheck.MinimumRunningSize)
+					ig.Status = model.HealthCheckError
+				}
+				if workingRate < ig.HealthCheck.MinimumRunningPercentage {
+					log.Warningf(appCtx, "Working Instance percentages %d is less than MinimumRunningPercentage %d\n", workingRate, ig.HealthCheck.MinimumRunningPercentage)
+					ig.Status = model.HealthCheckError
+				}
+			}
+			if ig.Status == model.HealthCheckError {
+				response = ctx.Accepted
+			} else {
+				response = ctx.Created
+			}
 		} else {
 			response = ctx.Created
 		}
+
 		_, err = hcStore.Update(appCtx, hc)
 		if err != nil {
-			log.Errorf(appCtx, "Working Instance percentages %d is less than MinimumRunningPercentage %d\n", workingRate, ig.HealthCheck.MinimumRunningPercentage)
+			log.Errorf(appCtx, "Failed to update InstanceGroupHealthCheck: %v because of %v\n", hc, err)
 			return err // Retry
 		}
 
