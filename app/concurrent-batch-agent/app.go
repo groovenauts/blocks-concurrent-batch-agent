3df6286d3d895464a7acb70b1d9e51a6bb8123be
akm <akm2000@gmail.com>
2019-09-20 11:05:44 +0900

    :recycle: Unify app/concurrent-batch-agent/*.go

--- app/concurrent-batch-agent/app.go
+++ app/concurrent-batch-agent/app.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	"admin"
+	"api"
+)
+
+func main() {
+	e := echo.New()
+	// note: we don't need to provide the middleware or static handlers, that's taken care of by the platform
+	// app engine has it's own "main" wrapper - we just need to hook echo into the default handler
+
+	admin.SetupRoutes(e, "admin/views")
+	api.SetupRoutes(e)
+
+	http.Handle("/", e)
+}
+
 // reference our echo instance and create it early
-var e = createMux()
